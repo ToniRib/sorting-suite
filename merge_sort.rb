ded5afae9c712565aab72f419ae9b5cc203a43e9
ToniRib <tonimarierib@gmail.com>
2015-10-15 12:47:55 -0600

    MergeSort: Adds a split in half method using each_slice

--- merge_sort.rb
+++ merge_sort.rb
@@ -7,6 +7,15 @@ class MergeSort
 
   def sort
     # use each_slice to split the array! see the enumerator class
-    []
+    sorted_array = []
+    
+  end
+
+  def split_in_half(arr)
+    if arr.length == 3
+      arr.each_slice(2).to_a
+    else
+      arr.each_slice(arr.length / 2).to_a
+    end
   end
 end

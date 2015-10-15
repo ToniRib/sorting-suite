455ea26e40d085f1ba546591e7b1fc594a616bbe
ToniRib <tonimarierib@gmail.com>
2015-10-15 15:20:21 -0600

    MergeSort: Modify split array into slice array and slightly modify the tests for it

--- merge_sort.rb
+++ merge_sort.rb
@@ -10,14 +10,14 @@ class MergeSort
   def sort
     # use each_slice to split the array! see the enumerator class
     sorted_array = []
-
+    sliced_array = slice_array
   end
 
-  def split_in_half(arr)
-    if arr.length == 3
-      arr.each_slice(2).to_a
+  def slice_array
+    if array.length > 2
+      sliced_array = array.each_slice(2).to_a
     else
-      arr.each_slice(arr.length / 2).to_a
+      slicd_array = array
     end
   end
 end

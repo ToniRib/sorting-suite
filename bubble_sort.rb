86974fc235f45e4e45732c9327f26a4bc80a59ca
ToniRib <tonimarierib@gmail.com>
2015-10-14 19:16:19 -0600

    Adds the first 5 tests for bubble sort

---
+++ bubble_sort.rb
@@ -0,0 +1,9 @@
+class BubbleSort
+  def initialize(array)
+    @array = array
+  end
+
+  def array
+    @array
+  end
+end
--- bubble_sort_test.rb
+++ bubble_sort_test.rb
@@ -1,5 +1,28 @@
 require 'minitest'
+require_relative 'bubble_sort'
 
 class BubbleSortTest < Minitest::Test
-  
+  def test_class_exists
+    assert BubbleSort, "Seems like BubbleSort doesn't exist yet!"
+  end
+
+  def test_can_initialize_a_sort
+    bubble = BubbleSort.new([])
+    assert_equal BubbleSort, bubble.class
+  end
+
+  def test_can_accept_an_array_of_numbers
+    bubble = BubbleSort.new([1, 2, 3])
+    assert_equal [1, 2, 3], bubble.array
+  end
+
+  def test_can_accept_an_array_of_letters
+    bubble = BubbleSort.new(['a', 'b', 'c'])
+    assert_equal ['a', 'b', 'c'], bubble.array
+  end
+
+  def test_can_sort_array_of_numbers
+    bubble = BubbleSort.new([5, 3, 7, 2])
+    assert_equal [2, 3, 5, 7], bubble.sort
+  end
 end

6837dff3fc7cfb59a6648590bba59d58034f3b0c
ToniRib <tonimarierib@gmail.com>
2015-10-16 17:42:47 -0600

    Adds additional tests for arrays of different lengths

--- spec/merge_sort_test.rb
+++ spec/merge_sort_test.rb
@@ -26,13 +26,30 @@ class MergeSortTest < Minitest::Test
     assert_equal [1], merge_sorter.merge_sort([1])
   end
 
-  def test_can_sort_an_array_of_numbers
+  def test_can_sort_an_array_of_two_numbers
+    merge_sorter = SortingSuite::MergeSort.new
+    assert_equal [1, 2], merge_sorter.merge_sort([2, 1])
+  end
+
+  def test_can_sort_an_array_of_three_numbers
+    skip
+    merge_sorter = SortingSuite::MergeSort.new
+    assert_equal [1, 2, 3], merge_sorter.merge_sort([3, 2, 1])
+  end
+
+  def test_can_sort_an_array_of_four_numbers
     merge_sorter = SortingSuite::MergeSort.new([3, 4, 2, 1])
     assert_equal [1, 2, 3, 4], merge_sorter.merge_sort([3, 4, 2, 1])
   end
 
-  def test_can_sort_a_large_array_of_numbers
+  def test_can_sort_an_array_of_eight_numbers
     merge_sorter = SortingSuite::MergeSort.new
     assert_equal [1, 2, 3, 4, 5, 7, 8, 9], merge_sorter.merge_sort([9, 3, 5, 1, 7, 8, 2, 4])
+  end
+
+  def test_can_sort_an_array_of_nine_numbers
+    skip
+    merge_sorter = SortingSuite::MergeSort.new
+    assert_equal [1, 2, 3, 4, 5, 6, 7, 8, 9], merge_sorter.merge_sort([8, 1, 4, 3, 9, 7, 2, 6, 5])
   end
 end

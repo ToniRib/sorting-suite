5ba9b78e1a67d876145433b9cb39a251b71bb0cf
ToniRib <tonimarierib@gmail.com>
2015-10-16 23:01:35 -0600

    Benchmark: Adds fastest, time, and calculate_time, plus a test for fastest. Couldn't add test for time method (since it varies) so added checks in the file itself for now.

--- lib/benchmark.rb
+++ lib/benchmark.rb
@@ -1,5 +1,40 @@
+require_relative 'bubble_sort'
+require_relative 'insertion_sort'
+require_relative 'merge_sort'
+
+require 'pry'
+
 module SortingSuite
   class Benchmark
+    def fastest(set = [])
+      bubble_time = calculate_time(SortingSuite::BubbleSort, set)
+      insertion_time = calculate_time(SortingSuite::BubbleSort, set)
+      merge_time = calculate_time(SortingSuite::BubbleSort, set)
 
+      if bubble_time < insertion_time && bubble_time < merge_time
+        'BubbleSort is the fastest!'
+      elsif insertion_time < bubble_time && insertion_time < merge_time
+        'InsertionSort is the fastest!'
+      else
+        'MergeSort is the fastest!'
+      end
+    end
+
+    def time(sort_type, set = [])
+      duration = calculate_time(sort_type, set)
+      "#{sort_type.to_s.split('::')[1]} took #{duration} seconds"
+    end
+
+    def calculate_time(sort_type, set = [])
+      t1 = Time.now
+      sort_type.new.sort(set)
+      t2 = Time.now
+      t2 - t1
+    end
   end
 end
+
+benchmark = SortingSuite::Benchmark.new
+set = (1..100).to_a.shuffle
+puts benchmark.time(SortingSuite::InsertionSort, set)
+puts benchmark.time(SortingSuite::MergeSort)
--- spec/benchmark_test.rb
+++ spec/benchmark_test.rb
@@ -8,7 +8,7 @@ class BenchmarkTest < Minitest::Test
 
   def test_can_determine_fastest_sort_method
     benchmark = SortingSuite::Benchmark.new
-    message = "MergeSort is the fastest!"
+    message = 'MergeSort is the fastest!'
     assert_equal message, benchmark.fastest([2, 8, 1, 0, 5])
   end
 end

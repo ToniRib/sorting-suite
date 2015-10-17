02960212b887366f8fe6f4921a7964ef45ac3b6a
ToniRib <tonimarierib@gmail.com>
2015-10-16 21:12:12 -0600

    MergeSort: Modifies merge_two_arrays to break if one array gets emptied (per the correct process). Still needs to be refactored.

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -15,36 +15,43 @@ module SortingSuite
     end
 
     def merge_two_arrays(arr1, arr2)
-      num_times = arr1.length + arr2.length - 1
-      num1 = arr1.shift
-      num2 = arr2.shift
+      return sort_pair(arr1[0], arr2[0]) if single?(arr1) && single?(arr2)
       new_array = []
-      num_times.times do |i|
-        new_array << return_smallest_number(num1, num2)
-        # get the next number
-        if num1 <= num2
-          if !arr1.empty?
-            num1 = arr1.shift
-          elsif !arr2.empty?
-            num1 = arr2.shift
-          else
-            new_array << num2
-          end
+      len1 = arr1.length
+      len2 = arr2.length
+      i = 0
+      j = 0
+      loop do
+        n1 = arr1[i]
+        n2 = arr2[j]
+        if n1 == return_smallest_number(n1, n2)
+          new_array << n1
+          i += 1
         else
-          if !arr2.empty?
-            num2 = arr2.shift
-          elsif !arr1.empty?
-            num2 = arr1.shift
-          else
-            new_array << num1
-          end
+          new_array << n2
+          j += 1
         end
+        break if i == len1 || j == len2
       end
-      new_array
+      if i == len1
+        new_array << arr2[j..len2]
+      else
+        j == len2
+        new_array << arr1[i..len1]
+      end
+      new_array.flatten
+    end
+
+    def single?(arr)
+      arr.length == 1
+    end
+
+    def sort_pair(num1, num2)
+      num1 <= num2 ? [num1, num2] : [num2, num1]
     end
 
     def return_smallest_number(num1, num2)
-      return num1 < num2 ? num1 : num2
+      return num1 <= num2 ? num1 : num2
     end
   end
 end

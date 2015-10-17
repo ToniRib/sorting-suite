b2a357d17eea7a9f23b93874c97d5d93696bf170
ToniRib <tonimarierib@gmail.com>
2015-10-17 10:33:29 -0600

    SelectionSort: Adds additional tests and creates the main algorithm for sorting

--- lib/selection_sort.rb
+++ lib/selection_sort.rb
@@ -1,7 +1,13 @@
 module SortingSuite
   class SelectionSort
-    def sort
-      []
+    def sort(arr = [])
+      return [] if arr.empty?
+      sorted_array = []
+      until arr.empty?
+        sorted_array << arr.min
+        arr.delete_at(arr.index(arr.min))
+      end
+      sorted_array
     end
   end
 end
--- spec/selection_sort_test.rb
+++ spec/selection_sort_test.rb
@@ -15,4 +15,29 @@ class SelectionSortTest < Minitest::Test
     selection_sorter = SortingSuite::SelectionSort.new
     assert_equal [], selection_sorter.sort
   end
+
+  def test_can_sort_a_single_digit_array
+    selection_sorter = SortingSuite::SelectionSort.new
+    assert_equal [1], selection_sorter.sort([1])
+  end
+
+  def test_can_sort_a_two_digit_array
+    selection_sorter = SortingSuite::SelectionSort.new
+    assert_equal [1, 2], selection_sorter.sort([2, 1])
+  end
+
+  def test_can_sort_an_array_of_numbers
+    selection_sorter = SortingSuite::SelectionSort.new
+    assert_equal [1, 2, 3, 4, 5, 6], selection_sorter.sort([5, 3, 6, 1, 2, 4])
+  end
+
+  def test_can_sort_an_array_with_duplicate_numbers
+    selection_sorter = SortingSuite::SelectionSort.new
+    assert_equal [1, 2, 3, 4, 4, 5, 6], selection_sorter.sort([5, 3, 6, 4, 1, 2, 4])
+  end
+
+  def test_can_sort_an_array_of_letters
+    selection_sorter = SortingSuite::SelectionSort.new
+    assert_equal %w(a b c d e), selection_sorter.sort(%w(e c d b a))
+  end
 end

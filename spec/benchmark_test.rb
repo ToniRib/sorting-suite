9b31572e5eb9fe5c5962a8faa8f1f88f11605ee7
ToniRib <tonimarierib@gmail.com>
2015-10-17 09:43:09 -0600

    Benchmark: Adds test for finding the slowest sort method

--- spec/benchmark_test.rb
+++ spec/benchmark_test.rb
@@ -11,4 +11,10 @@ class BenchmarkTest < Minitest::Test
     message = 'MergeSort is the fastest!'
     assert_equal message, benchmark.fastest([2, 8, 1, 0, 5])
   end
+
+  def test_can_determine_slowest_sort_method
+    benchmark = SortingSuite::Benchmark.new
+    message = 'BubbleSort is the slowest!'
+    assert_equal message, benchmark.slowest([1, 2, 3, 4, 5])
+  end
 end

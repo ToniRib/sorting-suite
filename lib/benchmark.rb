9a001f0ab63cbe41d6c1750f32fb94f0d5a69631
ToniRib <tonimarierib@gmail.com>
2015-10-17 10:10:26 -0600

    Benchmark: Clean up code

--- lib/benchmark.rb
+++ lib/benchmark.rb
@@ -2,8 +2,6 @@ require_relative 'bubble_sort'
 require_relative 'insertion_sort'
 require_relative 'merge_sort'
 
-require 'pry'
-
 module SortingSuite
   class Benchmark
     def fastest(set = [])
@@ -26,7 +24,8 @@ module SortingSuite
       bubble_time = calculate_time(SortingSuite::BubbleSort, set)
       insertion_time = calculate_time(SortingSuite::BubbleSort, set)
       merge_time = calculate_time(SortingSuite::BubbleSort, set)
-      {:bubble => bubble_time, :insertion => insertion_time, :merge => merge_time}
+
+      { bubble: bubble_time, insertion: insertion_time, merge: merge_time }
     end
 
     def return_message(sort_type, adjective)
@@ -45,7 +44,7 @@ module SortingSuite
       "#{sort_type.to_s.split('::')[1]} took #{duration} seconds"
     end
 
-    def calculate_time(sort_type, set = [])
+    def calculate_time(sort_type, set)
       t1 = Time.now
       sort_type.new.sort(set)
       t2 = Time.now
@@ -54,7 +53,7 @@ module SortingSuite
   end
 end
 
-benchmark = SortingSuite::Benchmark.new
-set = (1..100).to_a.shuffle
-puts benchmark.time(SortingSuite::InsertionSort, set)
-puts benchmark.time(SortingSuite::MergeSort)
+# benchmark = SortingSuite::Benchmark.new
+# set = (1..100).to_a.shuffle
+# puts benchmark.time(SortingSuite::InsertionSort, set)
+# puts benchmark.time(SortingSuite::MergeSort)

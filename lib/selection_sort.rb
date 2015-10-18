e42bb38ab8c27402e6e40f2a5323017b2bd99eb8
ToniRib <tonimarierib@gmail.com>
2015-10-18 11:50:15 -0600

    SelectionSort: Adds spacing to method

--- lib/selection_sort.rb
+++ lib/selection_sort.rb
@@ -2,11 +2,14 @@ module SortingSuite
   class SelectionSort
     def sort(arr = [])
       return [] if arr.empty?
+
       sorted = []
+
       until arr.empty?
         sorted << arr.min
         arr.delete_at(arr.index(arr.min))
       end
+
       sorted
     end
   end
